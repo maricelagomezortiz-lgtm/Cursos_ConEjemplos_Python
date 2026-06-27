2c661bbe14b13e953b2c92365db473bbb57fd54e
MariDev <maricela.datatecno.com.mx>
2026-06-27 12:34:33 -0600

    Inicio

---
+++ holamundo.py
@@ -0,0 +1,15 @@
+#Las variables se definen la primera vez que se nombran
+
+#Definimos variable
+mundo = "Pandora"
+habitantes = 1000000
+
+
+#Función basica para imprimir en pantalla
+print("Hola Mundo!")
+#Concatenando Valores
+print("Hola Mundo de",mundo)
+# con F'String para concatenar valores
+print(f"Hola Mundo de {mundo}") 
+#Formato Numerico con separador de miles
+print(f"Hola a los {habitantes:,} habitantes del Mundo de:{mundo}")
