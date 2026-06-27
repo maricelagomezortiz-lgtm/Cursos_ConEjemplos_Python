064471474dc44dd95784eb044e211580dcfbe782
MariDev <maricela.datatecno.com.mx>
2026-06-27 13:39:56 -0600

    area_circulo

---
+++ area_circulo.py
@@ -0,0 +1,16 @@
+#Importamos la libreria math para usar funciones matematicas
+import math 
+
+radio = 10
+radio_cuadrado = radio**2
+area= math.pi * radio_cuadrado
+
+#Imprimir en pantalla
+#A dos decimales
+#Con F'String
+print(f"El circulo con radio {radio:.2f} tiene un área de {area:.2f}")
+
+
+
+
+
